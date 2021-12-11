db6ef95974eaea5b43809a3921806891ad9b6b6e
Judy <747619498@qq.com>
2021-12-11 23:28:27 +0800

    简单设置路由

--- Ljy/src/app.js
+++ Ljy/src/app.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import add from "./images/start.png"
+import { Focus } from "./components/focus"
+import { User } from "./components/user"
+import { AddTodo } from "./components/addTodo"
+import { CountDown } from "./components/countDown"
+let nextId = 0;
 export const App = () => {
+    const [inputValue, setInputValue] = useState('test');
+    const [todoList, setTodoList] = useState([{
+        value: inputValue,
+        id: 0
+    }])
+    const handleChange = (e) => {
+        setInputValue(e.target.value)
+    }
+    const handleClick = () => {
+        nextId++;
+        setTodoList([...todoList, { value: inputValue, id: nextId }])
+        inputValue = ""
+    }
+
     return (
         <div>
             <Router>
-                <Route>
+                <Route path="/" exact render={() => (<Focus todoList={todoList} />)} />
+                <Route path="/user" component={User} />
+                <Route path="/additem" render={() => (<AddTodo onChange={handleChange} onClick={handleClick} value={inputValue} />)} />
+                <Route path="/countdown" component={CountDown} />
+                {/* <Route path="/" component={ } /> */}
 
-                </Route>
             </Router>
         </div>
     )
