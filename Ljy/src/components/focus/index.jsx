8e96619663f88f8ede58e5f70546c8f1766e0aea
Judy <747619498@qq.com>
2021-12-12 00:01:48 +0800

    focus 界面 title UI 完成

--- Ljy/src/components/focus/index.jsx
+++ Ljy/src/components/focus/index.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import add from "../../images/start.png"
-export const Focus = () => {
+import { AddItem } from '../addItem';
+import { AddTodo } from '../addTodo';
+import { Footer } from '../footer';
+import './style.css'
+export const Focus = (props) => {
+    const [isAdd, setIsAdd] = useState(false)
+    const Home = () => {
+        return (<div>
+            <div className="todoTitle">
+                <div className='titleContent'>
+                    <span>专注事项</span>
+                    <i className='addIcon'></i>
+                </div>
+            </div>
+            {/* <div id="todoList">
+                {props.todoList.map(() => <AddItem />)}
+            </div> */}
+            <Footer />
+        </div>)
+    }
     return (
         <div>
-            <div id="todoTitle">
-                <span>专注事项</span>
-                <img alt="" src={add} />
-            </div>
-            <div id="todoList">
-
-            </div>
-            <Footer />
+            {isAdd ? <AddTodo /> : <Home />}
         </div>
+
     )
 }
