7fbb881ac8a2c03b0f3c67a7fe6c614364dca7fc
Judy <747619498@qq.com>
2021-12-12 01:00:19 +0800

    添加 todoItem 和 addItem 转换

--- Ljy/src/components/focus/index.jsx
+++ Ljy/src/components/focus/index.jsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
-import add from "../../images/start.png"
 import { AddItem } from '../addItem';
 import { AddTodo } from '../addTodo';
 import { Footer } from '../footer';
 import './style.css'
 export const Focus = (props) => {
     const [isAdd, setIsAdd] = useState(false)
+
+    const handleSubmit = (value) => {
+        alert(value)
+        setIsAdd(!isAdd)
+    }
+
     const Home = () => {
         return (<div>
             <div className="todoTitle">
                 <div className='titleContent'>
                     <span>专注事项</span>
-                    <i className='addIcon'></i>
+                    <i className='addIcon' onClick={() => setIsAdd(!isAdd)}></i>
                 </div>
             </div>
+            <main>
+                <div className='todoItems'>
+                    {props.todoList.map(todoItem => <AddItem content={todoItem.value} />)}
+                </div>
+            </main>
             {/* <div id="todoList">
                 {props.todoList.map(() => <AddItem />)}
             </div> */}
@@ -22,7 +32,7 @@ export const Focus = (props) => {
     }
     return (
         <div>
-            {isAdd ? <AddTodo /> : <Home />}
+            {isAdd ? <AddTodo handleSubmit={handleSubmit} /> : <Home />}
         </div>
 
     )
