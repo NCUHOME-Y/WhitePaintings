dfc0f04840c519b8dc400c18232c733049b34c7e
Judy <747619498@qq.com>
2021-12-12 00:19:09 +0800

    footer UI 完成

--- Ljy/src/components/footer/index.jsx
+++ Ljy/src/components/footer/index.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import file from "../../images/file.png"
 import club from "../../images/club.png"
 import my from "../../images/my.png"
+import "./style.css"
 
 export const Footer = () => {
-    <div className="footer">
-        <img alt="" sec={file} />
-        <img alt="" sec={club} />
-        <img alt="" sec={my} />
-    </div>
+    return (
+        <div className="footer">
+            <i className='footer-focus'></i>
+            <i className='footer-home'></i>
+            <i className='footer-user'></i>
+        </div>
+    )
 }
+<i></i>
