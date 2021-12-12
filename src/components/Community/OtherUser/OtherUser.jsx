ef660cb0425fe950cd34532c3aa4cadb7be3f166
Judy <747619498@qq.com>
2021-12-12 15:14:53 +0800

    添加并完成了社区界面

---
+++ src/components/Community/OtherUser/OtherUser.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class OtherUser extends Component {
+    render(){
+        return(
+            <div>
+                <div className="otherUserFace"></div>
+            </div>
+        )
+    }
+}
