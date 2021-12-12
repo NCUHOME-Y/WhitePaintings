448a11ee575b0ed9c5ad29e79d3387ea74a15152
Judy <747619498@qq.com>
2021-12-12 13:59:38 +0800

    修复了倒计时

--- src/components/countDown/index.jsx
+++ src/components/countDown/index.jsx
@@ -1,18 +1,57 @@
-import React from 'react';
-import { Footer } from '../footer';
-import stop from "../../images/stop.png"
+import React, { Component } from 'react';
+import "./style.css"
 import board from "../../images/board.png"
-
-export const CountDown = (params) => {
-    return (
-        <div>
-            <div id="countDownTitle">
-                <h3>写高数作业</h3>
-                <h3>time</h3>
-                <img alt='' src={stop} />
+import stop from "../../images/stop.png"
+import { Footer } from '../footer'
+export default class CountDown extends Component {
+    constructor(props) {
+        super(props)
+        let hour = Math.floor(props.duration / 3600)
+        let minute = Math.floor((props.duration - hour * 3600) / 60)
+        let second = (props.duration - hour * 3600 - minute * 60)
+        this.state = {
+            hour: hour < 10 ? '0' + hour : hour,
+            minute: minute < 10 ? '0' + minute : minute,
+            second: second < 10 ? '0' + second : second
+        };
+    }
+    componentDidMount() {
+        if (this.props.duration) {
+            let date = new Date();
+            let endTime = date.setSeconds(date.getSeconds() + Number(this.props.duration));
+            this.countFun(endTime);
+        }
+    }
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+    countFun = (endTime) => {
+        let time = endTime - new Date().getTime();
+        this.timer = setInterval(() => {
+            if (time > 1000) {
+                time -= 1000;
+                let hour = Math.floor((time / 1000 / 3600) % 24);
+                let minute = Math.floor((time / 1000 / 60) % 60);
+                let second = Math.floor((time / 1000) % 60);
+                this.setState({
+                    hour: hour < 10 ? '0' + hour : hour,
+                    minute: minute < 10 ? '0' + minute : minute,
+                    second: second < 10 ? '0' + second : second,
+                });
+            } else clearInterval(this.timer);
+        }, 1000);
+    };
+    render() {
+        return (
+            <div id="countDownBox">
+                <div id="countDownTitle">
+                    <h1>写高数作业</h1>
+                    <h2 id="countDownTime">{this.state.hour}:{this.state.minute}:{this.state.second}</h2>
+                    <button id="countDownStop"><img alt="" src={stop} width="70px" height="70px" /></button>
+                </div>
+                <img id="board" alt="" src={board} width="244px" />
+                <Footer />
             </div>
-            <img alt='' src={board} />
-            <Footer />
-        </div>
-    )
+        );
+    }
 }
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,13 +3,14 @@ import file from "../../images/file.png"
 import club from "../../images/club.png"
 import my from "../../images/my.png"
 import "./style.css"
+import { Link } from 'react-router-dom';
 
 export const Footer = () => {
     return (
         <div className="footer">
-            <i className='footer-focus'></i>
-            <i className='footer-home'></i>
-            <i className='footer-user'></i>
+            <Link to='/'> <i className='footer-focus'></i></Link>
+            <Link to='/club'><i className='footer-home'></i></Link>
+            <Link to='/user'> <i className='footer-user'></i></Link>
         </div>
     )
 }
