9fd7844f92b7926bb145ff46a654f997c0473dfd
Judy <747619498@qq.com>
2021-12-12 15:29:17 +0800

    完成了排行榜样式

--- src/components/Community/Rankinglist/Rankinglist.jsx
+++ src/components/Community/Rankinglist/Rankinglist.jsx
@@ -2,16 +2,16 @@ import React, { Component } from 'react'
 import Rank from '../Rank/Rank'
 
 export default class Rankinglist extends Component {
-    render(){
-        const {rank} = this.props
-        return(
-            <div className='Rankinglist'>
+    render() {
+        const { rank } = this.props
+        return (
+            <ul className='Rankinglist'>
                 {
-                    rank.map((rank)=>{
-                        return <Rank key={rank.id} {...rank}/>
+                    rank.map((rank) => {
+                        return <Rank key={rank.id} {...rank} />
                     })
                 }
-            </div>
+            </ul>
         )
     }
 }
