2ed769ec1372ced95db7e83e99d1af3417586403
Judy <747619498@qq.com>
2021-12-12 09:08:31 +0800

    完成了添加页面

--- Ljy/src/components/addTodo/index.jsx
+++ Ljy/src/components/addTodo/index.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-export const AddTodo = (params) => {
+import React, { useState } from 'react';
+import retBtn from '../../images/retBtn.png'
+import "./style.css"
+export const AddTodo = ({ handleSubmit }) => {
+    const [value, setValue] = useState("")
     return (
-        <div>
-            <div id="addTitle">
-                <img alt="" src={ } />
-                <h6>添加事项</h6>
-                <button>完成</button>
-            </div>
-            <div id="addValue">
-                <h5>任务名称</h5>
-                <input />
-                <h5>专注时间</h5>
-                <select>
-                    <option>25分钟</option>
-                    <option>50分钟</option>
-                    <option>75分钟</option>
-                </select>
+        <div className='addBox'>
+            <div className='addPage'>
+                <div className="addTitle">
+                    <img alt="" src={retBtn} width="30px" height="30px" />
+                    <h6>添加事项</h6>
+                    <button onClick={() => handleSubmit(value)}>完成</button>
+                </div>
+                <div className="addValue">
+                    <h5>任务名称</h5>
+                    <input onChange={(e) => setValue(e.target.value)} value={value} />
+                    <h5>专注时间</h5>
+                    <select>
+                        <option>25分钟</option>
+                        <option>50分钟</option>
+                        <option>75分钟</option>
+                    </select>
+                </div>
             </div>
         </div>
     )
