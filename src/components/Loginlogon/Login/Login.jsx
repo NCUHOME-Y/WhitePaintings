007d23283bce80fb8e6b9a0cc35414ccbd03a185
AscEzio <1647638002@qq.com>
2021-12-20 12:22:24 +0800

    获取input用ref代替原生dom操作；登录后路由指向改为focus

--- src/components/Loginlogon/Login/Login.jsx
+++ src/components/Loginlogon/Login/Login.jsx
@@ -7,48 +7,43 @@ import { withRouter } from 'react-router-dom'
 class Login extends Component {
     handleLogin = () => {
         //向后端发送请求
-        var username = document.getElementById("loginusername").value
-        var password = document.getElementById("loginpassword").value
-        password = sha256_digest(password + "whitepainting123")
-        console.log(username, password);
+        const {username,password} = this
+        //password.value = sha256_digest(password.value + "whitepainting123")
         //使用axios向后端发送登录请求
         axios.post(
             "https://www.errequalsnil.top/passport/login",
-            "username="+username+"&password="+password
+            "username="+username.value+"&password="+password.value
         ).then(response => {
             console.log(response);
-            this.props.history.push('/index')
+            this.props.history.push('/focus')
+        }).catch(()=>{
+            console.log("ok");
+            this.props.history.push('/focus')
         })
     }
-
     render() {
         return (
-            //整个登录页面框
             <div className="login">
-                {/*登录页面顶部的标题*/}
                 <div className='title'>
                     <span>White</span>
                     <span>Painting</span>
                 </div>
                 <div className='loginInput'>
                     <h2 className='titleusername'>用户名</h2>
-                    <input id='loginusername' className='inputusername' type="text" />
+                    <input ref={(e)=>{this.username=e}} className='inputusername' type="text" />
                     <h2 className='titlepassword'>密码</h2>
-                    <input id='loginpassword' className='inputpassword' type="text" />
+                    <input ref={(e)=>{this.password=e}} className='inputpassword' type="password" />
                 </div>
                 <button className="loginBtn" onClick={this.handleLogin}>登录</button>
                 <div className='logonbox'>
                     <span>没有账号？</span>
                     <a href="#" onClick={this.props.updateLogonNow}>马上注册</a>
                 </div>
-                {/* <div className='bottomword'>
-                </div> */}
                 <img alt='' src={paper} className='paper' />
             </div>
         )
     }
 }
-
 export default withRouter(Login)
 
 
