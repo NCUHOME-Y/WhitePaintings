5b394c896cc9e764b0976c97ea039aa2afb84ab9
Judy <747619498@qq.com>
2021-12-21 23:08:36 +0800

    修改了user界面css

--- src/components/user/index.jsx
+++ src/components/user/index.jsx
@@ -4,7 +4,7 @@ import { Footer } from '../footer';
 import reBtnR from '../../images/retBtnR.png'
 import "./style.css"
 import { Link } from 'react-router-dom';
-export const User = (params) => {
+export const User = () => {
     return (
         <div className='userBox'>
             <div className="userTitel">
@@ -12,10 +12,10 @@ export const User = (params) => {
                 <h6>还没想好用户名</h6>
             </div>
             <ul className="userList">
-                <Link to='/message'><li>系统消息 <img alt='' src={reBtnR} /></li></Link>
-                <li>我的专注分享 <img alt='' src={reBtnR} /></li>
+                <Link to='/message' ><li>系统消息 <img alt='' src={reBtnR} /></li></Link>
+                <Link to='/mytotalshare'><li >我的专注分享 <img alt='' src={reBtnR} /></li></Link>
                 <li>详细专注数据 <img alt='' src={reBtnR} /></li>
-                <Link to='/userchange'><li>修改个人信息 <img alt='' src={reBtnR} /></li></Link>
+                <Link to='/userchange' ><li>修改个人信息 <img alt='' src={reBtnR} /></li></Link>
             </ul>
             <Footer />
         </div>
