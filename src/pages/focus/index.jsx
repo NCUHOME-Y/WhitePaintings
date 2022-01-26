5e60f977e591c87f34c08dbda98cc4c5406a8bf4
Judy <747619498@qq.com>
2022-01-26 18:16:57 +0800

    修复了刷新界面数据丢失的问题

--- src/pages/focus/index.jsx
+++ src/pages/focus/index.jsx
@@ -1,56 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AddItem } from './components/addItem';
 import { AddTodo } from './components/addTodo';
 import { Footer } from '../../components/footer';
 import './style.css'
 let nextId = 0;
 export const Focus = () => {
-    // const [inputValue, setInputValue] = useState('test');
-    // const [todoList, setTodoList] = useState([{
-    //     value: "xiaoliu",
-    //     id: 0
-    // }, {
-    //     value: "rao",
-    //     id: 0
-    // }])
     const [isAdd, setIsAdd] = useState(false)
-    // const handleSubmit = (value) => {
-    //     // alert(value)
-    //     setInputValue(value)
-    //     setIsAdd(!isAdd)
-    //     setTodoList([...todoList, { value: inputValue, id: nextId }])
-    // }
-
-
     const [inputValue, setInputValue] = useState('');
-    const [todoList, setTodoList] = useState([
-        {
-            id: 0,
-            content: '写高数作业',
-            done: false
-        }
-    ])
+    const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem('todoList') || "[]"))
     const handleChange = (e) => {
         console.log(e)
         setInputValue(e.target.value)
         console.log(inputValue)
     }
-    // if (inputValue !== '') {
-    //     ifInvolve = false;
-    // }
     const hanldeClick = () => {
-        // setInputValue('')
         setIsAdd(!isAdd)
         setTodoList(
             [...todoList,
             {
-                id: nextId++,
+                id: ++nextId,
                 content: inputValue,
                 done: false
             }]
         )
-        console.log("id:", nextId)
+        console.log(todoList)
     }
+    useEffect(
+        () => {
+            localStorage.setItem('todoList', JSON.stringify(todoList));
+            return () => console.log(123);
+        }, [todoList]
+    );
     const Home = () => {
         return (<div className='focusContainer'>
             <div className="todoTitle">
